c99bb391c82849e6a58ff4ca531b8a970fd59422
Your Name <you@example.com>
2024-10-28 13:02:37 +0800

    1

--- test.c
+++ test.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
-//#include<stdlib.h>
-//#include<string.h>
-int i = 1;
-static int j = 15;
-int func() {
-    int i = 10;
-    if (i > 5) i++;
-    printf("i = %d, j = %d\n", i, j);
-    return i % j;
-}
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SWAP(a, b, t) t = a; a = b; b = t
+#define SQUARE(a) a *a
+#define SWAPWHEN(a, b, t, cond) if (cond) SWAP(a, b, t)
 int main() {
-    int a = func();
-    printf("a = %d\n", a);
-    printf("i = %d, j = %d\n", i, j);
-    return 0;
+    int tmp;
+    int x = 1;
+    int y = 2;
+    int z = 3;
+    int w = 3;
+    SWAP(x, y, tmp);
+    printf("x = %d, y = %d, tmp = %d\n", x, y, tmp);
+    if (x y) SWAP(x, y, tmp);
+    printf("x = %d, y = %d, tmp = %d\n", x, y, tmp);
+    SWAPWHEN(x, y, tmp, SQUARE(1 + 2 + z++ + ++w) == 100);
+    printf("x = %d, y = %d\n", x, y, tmp);
+    printf("z = %d, w = %d, tmp = %d\n", z, w, tmp);
 }
