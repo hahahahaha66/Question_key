58a9da3e1edef8f19b2274fb7a5bfd3b2b3ddf67
Your Name <a310608318@gmail.com>
2024-11-08 21:37:25 +0800

    11.8

--- C Primer Plus/第十五章/15.5.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    unsigned int num,a;
-    scanf("%u,%u",&num,&a);
-    unsigned int p=num&((1<<a)-1);
-    num>>=a;
-    num|=(p<<(sizeof(num)*8-a));
-    return 0;
-}
